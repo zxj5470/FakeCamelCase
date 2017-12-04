55c63a95d02ebe5285228453f497f6ad26730d87
zxj5470 <zxj5470@foxmail.com>
2017-12-04 18:22:44 +0800

    likes worked As♂s We can!!!! Version 1.0 works

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # FakeCamelCase
+
+## a fake looks for lowerCamelCase
+
+- In Rider version since `RD-172.4144.1459`
 ![pic1](images/pic1.jpg)
-![pic2](images/pic2.jpg)
+
+
+> Now it works !
+> Because I crate a fake `C# File` class for `plugin.xml` 
+
+![pic2](images/pic3.jpg)
--- src/main/java/com/jetbrains/rider/ideaInterop/fileTypes/csharp/CSharpLang.java
+++ src/main/java/com/jetbrains/rider/ideaInterop/fileTypes/csharp/CSharpLang.java
@@ -1,12 +1,14 @@
-package com.github.zxj5470.fcc.utils;
+package com.jetbrains.rider.ideaInterop.fileTypes.csharp;
 
-import com.intellij.lang.Language;
 import com.jetbrains.rider.ideaInterop.fileTypes.RiderLanguageBase;
 
 public class CSharpLang extends RiderLanguageBase {
-
     protected CSharpLang() {
         super("C#", "CSHARP");
     }
 
+    @Override
+    public boolean isCaseSensitive() {
+        return true;
+    }
 }
